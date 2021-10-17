e56b64eb000dadede37eba23daa52a8d085ff6cc
yusuftask08 <yusuftaskiran1@gmail.com>
2021-10-17 15:51:55 +0300

    loading component

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,35 @@
-import React, { Component, Fragment } from "react";
-import Navbar from "./Navbar";
-import Users from "./Users";
-import axios from "axios";
-export class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      users: [],
-    };
-  }
-  componentDidMount() {
-    axios.get("https://api.github.com/users").then((_) => {
-      this.setState({ users: _.data });
-    });
-  }
+import React, { Component, Fragment } from 'react'
+import Navbar from './Navbar'
+import User from './User'
+import Users from './Users'
+import axios from 'axios'
 
-  render() {
-    return (
-      <>
-        <Navbar />
-        <Users users={this.state.users} />
-      </>
-    );
-  }
+export class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: false,
+            users: []
+        }
+    }
+    componentDidMount() {
+        this.setState({loading: true});
+        
+        setTimeout(() => {
+            axios
+                .get('https://api.github.com/users')
+                .then(res => this.setState({users: res.data, loading: false}));
+        }, 3000);
+        
+    }
+    render() {
+        return (
+            <>
+                <Navbar />
+                <Users users={this.state.users} loading={this.state.loading}/>
+            </>
+        )
+    }
 }
 
-export default App;
+export default App
--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,12 @@ module.exports = {
             {
                 test: /\.scss$/,
                 use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [{
+                    loader: 'file-loader',
+                }]
             }
         ]
     }
