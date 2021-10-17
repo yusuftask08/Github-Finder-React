3875aa05467d61229ee9f7fcff76f51d39cc63a8
yusuftask08 <yusuftaskiran1@gmail.com>
2021-10-17 15:17:16 +0300

    Users component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React, { Component, Fragment } from "react";
 import Navbar from "./Navbar";
-import User from "./User";
+import Users from "./Users";
 export class App extends Component {
   render() {
     return (
       <>
         <Navbar />
-        <User />
+        <Users />
       </>
     );
   }
--- src/components/User.js
+++ src/components/User.js
@@ -1,35 +1,17 @@
 import React, { Component } from "react";
 
 export class User extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      id: "52704572",
-      name: "Yusuf Taskiran",
-      login: "yusuftask08",
-      html_url: "https://github.com/yusuftask08",
-      avatar_url: "https://avatars.githubusercontent.com/u/52704572?v=4",
-      followers: 19,
-    };
-  }
   render() {
-    const { name, login, avatar_url, html_url, followers } = this.state;
+    const { login, avatar_url, html_url } = this.props.user;
     return (
-      <div>
-        <div className="card m-2">
-          <div className="row">
-            <div className="col-md-3">
-              <img src={avatar_url} alt="" className="card-img" />
-            </div>
-            <div className="col-md-9">
-              <div className="card-body">
-                <h5 className="card-title">{name}</h5>
-                <p className="card-text">Followers : {followers} </p>
-                <a href={html_url} className="btn btn-dark btn-sm">
-                  Go Profile
-                </a>
-              </div>
-            </div>
+      <div className="col-md-4 col-sm-6 col-lg-3">
+        <div className="card mt-2">
+          <img src={avatar_url} alt="" className="img-fluid" />
+          <div className="card-body">
+            <h5 className="card-title">{login}</h5>
+            <a href={html_url} className="btn btn-primary btn-sm">
+              Go Profile
+            </a>
           </div>
         </div>
       </div>
