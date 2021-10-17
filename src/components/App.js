73aa00ebce9ac10222a117b305d230968fcdd7d2
yusuftask08 <yusuftaskiran1@gmail.com>
2021-10-17 15:27:37 +0300

    http request

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
 import React, { Component, Fragment } from "react";
 import Navbar from "./Navbar";
 import Users from "./Users";
+import axios from "axios";
 export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+    };
+  }
+  componentDidMount() {
+    axios.get("https://api.github.com/users").then((_) => {
+      this.setState({ users: _.data });
+    });
+  }
+
   render() {
     return (
       <>
         <Navbar />
-        <Users />
+        <Users users={this.state.users} />
       </>
     );
   }
